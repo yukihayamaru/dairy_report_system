cbe2028b9f8e6b9eb9940edabb9fde0e318c7b1d
hayamaruhiroshiakira <hayamaruhiroshiakira@192.168.0.23>
2021-12-12 21:03:04 +0900

    FrontController作成

---
+++ src/main/java/actions/UnknownAction.java
@@ -0,0 +1,23 @@
+package actions;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import constants.ForwardConst;
+
+/**
+*エラ〜発生時の処理を行うことがActionクラス
+*
+*/
+public class UnknownAction extends ActionBase{
+    /**
+    *共通エラー画面「お探しのページは見つかりませんでした。」を表示する
+    */
+    @Override
+    public void process()throws ServletException,IOException{
+
+        //エラー画面を表示
+        forward(ForwardConst.FW_ERR_UNKNOWN);
+        }
+    }
